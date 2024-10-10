82e400a7a7b6967cb6178a36e11dd44e78a3b58f
Damianen <113175544+Damianen@users.noreply.github.com>
2024-10-10 11:38:59 +0200

    Add index

--- voedselverspilling.UI/Controllers/HomeController.cs
+++ voedselverspilling.UI/Controllers/HomeController.cs
@@ -1,12 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
-using voedselverspilling.Domain.Models;
 
 namespace voedselverspilling.Controllers {
 
 public class HomeController : Controller
 {
-    Student st = new Student();
-
     public IActionResult Index()
     {
         return View();
