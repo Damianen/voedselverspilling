a03da6beeb192ac8cfe8de00f3555df129cb3b95
Damianen <113175544+Damianen@users.noreply.github.com>
2024-10-20 19:40:06 +0200

    Add Package pages and account pages

--- voedselverspilling.UI/Controllers/AccountController.cs
+++ voedselverspilling.UI/Controllers/AccountController.cs
@@ -1,10 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace voedselverspilling.Controllers {
+namespace voedselverspilling.UI.Controllers {
 
 public class AccountController : Controller
 {
+    public IActionResult Index()
+    {
+        return View();
+    }
+
     public IActionResult Login()
+    {
+        return View();
+    }
+
+    public IActionResult Account()
+    {
+        return View();
+    }
+
+    public IActionResult Create()
     {
         return View();
     }
---
+++ voedselverspilling.UI/Controllers/PackageController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace voedselverspilling.UI.Controllers {
+
+public class PackageController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Package()
+    {
+        return View();
+    }
+}
+
+}
